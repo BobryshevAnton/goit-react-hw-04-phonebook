00a0061048e147cdc646cb62e0461784c8ba07d2
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2023-03-10 15:24:45 +0100

    change contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,16 @@ import { Modal } from './Modal/Modal';
 import css from './app.module.css';
 import { nanoid } from 'nanoid';
 
-const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('phoneBook') ?? []);
-  });
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('phoneBook')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
+
   const [filter, setFilter] = useState('');
   const [showModal, setShowModal] = useState(false);
 
